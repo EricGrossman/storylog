cf1c0bfc0802d6e097b88be2f57498f464dbb16c
Eric Grossman <ejm.grossman@gmail.com>
2013-09-12 23:05:38 -0400

    Added authorized? method (hack)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :authorized?
   
   def current_user
     if session["user_id"]
@@ -18,4 +18,17 @@ class ApplicationController < ActionController::Base
 	end
   end
   
+  def authorized?
+    if session["user_id"] && current_user
+	  #Assumed that the first 2 users are me...Hack.
+	  #I'm learning.
+	  if current_user.uid.to_i < 3
+	    return true
+	  else
+	    return false
+	  end
+	else
+	  return false
+	end
+  end
 end
